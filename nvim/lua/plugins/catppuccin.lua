d79393ce37196b0c03c7c497cedbeb780e49893a
Aditya Vishwakarma <adityavishwakarma69+con@protonmail.com>
2024-09-09 16:16:29 +0530

    neovim config added

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,10 @@
+local plugin = {
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme "catppuccin"
+  end
+}
+return plugin
